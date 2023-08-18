a930cafa225ee71a7214c8735e5cf3d589a29f94
ngtram1 <vo9418172@gmail.com>
2023-08-18 09:50:47 +0700

    init project

---
+++ bai2_animatedsidebarmenu/android/app/src/main/kotlin/com/example/bai2_animatedsidebarmenu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bai2_animatedsidebarmenu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bai4_loginpage/android/app/src/main/kotlin/com/example/bai4_loginpage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bai4_loginpage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
